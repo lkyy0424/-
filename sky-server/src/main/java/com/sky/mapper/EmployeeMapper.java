ced246d05dede79ebf420eabb8c6449ae0531983
jj <jz3152948213@gmail.com>
2025-09-14 06:45:07 +0900

    外卖新增员工代码

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,7 +1,9 @@
 package com.sky.mapper;
 
 import com.sky.entity.Employee;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
@@ -14,5 +16,11 @@ public interface EmployeeMapper {
      */
     @Select("select * from employee where username = #{username}")
     Employee getByUsername(String username);
+    @Insert("INSERT INTO employee " +
+            "(name, username, password, phone, sex, id_number, status, create_time, update_time, create_user, update_user) " +
+            "VALUES " +
+            "(#{name}, #{username}, #{password}, #{phone}, #{sex}, #{idNumber}, #{status}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser})")
+//    @Options(useGeneratedKeys = true, keyProperty = "id")
+    int insert(Employee employee);
 
 }
